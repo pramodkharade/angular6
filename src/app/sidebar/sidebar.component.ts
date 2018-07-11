df8f77bf1ca59fdfdb239c50c4161ea0dda06690
pramod.kharade <pramod.kharade@blazeclan.com>
2018-07-11 15:26:27 +0530

    activated sidebar menu

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, NavigationEnd } from "@angular/router";
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
-  constructor() { }
+currentUrl:string;
+  constructor(private router:Router) { 
+    router.events.subscribe((_:NavigationEnd)=>this.currentUrl=_.url)
+  }
 
   ngOnInit() {
   }
