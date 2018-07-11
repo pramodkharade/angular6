f3191bf66a16bc8d4c43e48bb75682e143ef8044
pramod.kharade <pramod.kharade@blazeclan.com>
2018-07-11 11:00:45 +0530

    default with component sidebar,posts,user

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from "./users/users.component";
+import { DetailsComponent } from "./details/details.component";
+import { PostsComponent } from "./posts/posts.component";
+//import { SidebarComponent} from "./sidebar/sidebar.component";
+
+const routes: Routes = [
+  {
+    path:"",
+    component:UsersComponent
+  },
+  {
+    path:"details/:id",
+    component: DetailsComponent
+  },
+  {
+    path:"posts",
+    component: PostsComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
